965aca797e90e3178cd405dfa350e257a798c96c
Fayyaz Alam <fayazalam.98@gmail.com>
2021-07-14 12:17:28 -0700

    all code pushed once ;/

---
+++ app.js
@@ -0,0 +1,55 @@
+function localStr() {
+    var name = document.getElementById("inputName")
+    var number = document.getElementById("inputNumber")
+    var email = document.getElementById("inputEmail")
+    var password = document.getElementById("inputPassword")
+
+    var emailc = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    var userc = /^[a-z0-9_-]{3,16}$/;
+    var numc = /^((\+92)|(0092))-{0,1}\d{3}-{0,1}\d{7}$|^\d{11}$|^\d{4}-\d{7}$/;
+    var passc = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+
+    if (userc.test(name.value)) {
+        if (numc.test(number.value)) {
+            if (emailc.test(email.value)) {
+                if (passc.test(password.value)) {
+
+                    var userDetail = {
+                        name: name.value,
+                        number: number.value,
+                        email: email.value,
+                        password: password.value
+                    }
+                    localStorage.setItem('userDetail', JSON.stringify(userDetail))
+
+                   if (true) {
+                    location.href ="login.html"; 
+                   }
+                }
+                else{
+                    alert("pass check")
+                }
+               
+
+            }
+            else {
+                alert('check email')
+            }
+
+        }
+        else {
+            alert('check number')
+        }
+
+
+    }
+
+
+    else {
+        alert(' username check')
+
+    }
+   
+}
+
+
