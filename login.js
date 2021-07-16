c54a5b80771afcf401655f3e23a343e3d07eb7e8
Fayyaz Alam <fayazalam.98@gmail.com>
2021-07-16 06:51:30 -0700

    if theere is data in local storage use it else shows alert

--- login.js
+++ login.js
@@ -5,5 +5,15 @@ function gotohome() {
     var userPass = document.getElementById('inputPassword').value;
     let usersData = JSON.parse(localStorage.getItem('allUsers'))
 
+    let flag = false
+if(usersData){
+    for (let a = 0; a < usersData.length; a++) {
+        if (userName == usersData[a].email && userPass == usersData[a].password) {
+            flag = true
+        }
+    }
+}
+else{
+    alert("no Data found")
 
 }
