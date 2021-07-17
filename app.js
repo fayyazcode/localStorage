74328cff7632463ea536580dba4ddeb9f7e21d90
fayyazcode <fayazalam.98@gmail.com>
2021-07-17 10:37:47 +0500

    form removed

--- app.js
+++ app.js
@@ -33,8 +33,7 @@ function localStr() {
                    
                     for (i = 0; i < allUsers.length; i++) {
                         if (allUsers[i].email === email.value) {
-                            flag = true
-        
+                            flag = true       
                         }
                     }
 
